b12170c0e9cc5d35cb705ef33e6c229938fe0a6b
Serhii Bratkov <sir3720@gmail.com>
2025-07-14 03:10:52 +0300

    fix new movieService

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,30 +1,10 @@
 import axios from "axios";
 import type { Movie } from "../types/movie.ts";
 
-const BEARER_KEY = import.meta.env.VITE_TMDB_API_KEY;
-const BASE_URL = import.meta.env.VITE_TMDB_BASE_URL;
-const SEARCH_MOVIE_ENDPOINT = "/search/movie";
-
-export const TMDB_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
-export const TMDB_IMAGE_ORIGINAL_URL = "https://image.tmdb.org/t/p/original";
-
-export const getImageUrl = (
-  imagePath: string | null,
-  size: "w500" | "original" = "w500"
-): string => {
-  if (!imagePath) return "";
-  const baseUrl =
-    size === "original"
-      ? "https://image.tmdb.org/t/p/original"
-      : "https://image.tmdb.org/t/p/w500";
-  return `${baseUrl}${imagePath}`;
-};
-
 const api = axios.create({
-  baseURL: BASE_URL,
+  baseURL: import.meta.env.VITE_TMDB_BASE_URL,
   headers: {
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${BEARER_KEY}`,
+    Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_KEY}`,
   },
 });
 
@@ -35,18 +15,16 @@ interface MoviesResponse {
   total_results: number;
 }
 
+export const getImageUrl = (
+  imagePath: string | null,
+  size: "w500" | "original" = "w500"
+): string =>
+  imagePath ? `https://image.tmdb.org/t/p/${size}${imagePath}` : "";
+
 export const fetchMovie = async (
   query: string,
-  page: string = "1"
+  page = "1"
 ): Promise<MoviesResponse> => {
-  const urlSearchParams = new URLSearchParams({
-    query,
-    page,
-  });
-
-  const { data } = await api.get<MoviesResponse>(
-    `${SEARCH_MOVIE_ENDPOINT}?${urlSearchParams.toString()}`
-  );
-
+  const { data } = await api.get("/search/movie", { params: { query, page } });
   return data;
 };
